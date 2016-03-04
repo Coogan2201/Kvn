378179f55d165628ec03c347963ebeff40ffdbfd
Coogan2201 <c.coogan2201@gmail.com>
2016-03-04 18:00:49 -0500

    Update Master.ino

--- ArmGripper/Master.ino
+++ ArmGripper/Master.ino
@@ -1,43 +1,76 @@
 // Master: Arm Arduino
 
 #include <Wire.h>
-#include <Servo.h>
-String mot;
-String dis;
-String dir;
-String del;
-Servo myservo;  // create servo object to control a servo
-// twelve servo objects can be created on most boards
-int pos = 0;
-int wellDown=0;
-void setup() {
-  // Start the I2C Bus as Master
+int incomingByte;
+
+void setup() 
+{
   Wire.begin();
   Serial.begin(9600);                                         // Initialize serial communication
-
 }
-void loop() {
+void loop() 
+{
   Wire.beginTransmission(9); // transmit to device #9
-   while (Serial.available()>0) 
+  while (Serial.available()>0) 
   {
-    mot  = Serial.readStringUntil(':');
-    Serial.read();
-    dir = Serial.readStringUntil(':');                            // Read data from serial port
-    Serial.read();
-    dis  = Serial.readStringUntil('\0');
-Serial.println(mot);
-Serial.println(wellDown);
-if(mot=="Well plates")                                                    // Find the wells in a plate
-    {
-      Wire.write(1);              // sends x 
-    }
+    incomingByte = Serial.read();
   }
-  if(mot=="Well platess")
+  switch(incomingByte)
   {
-    Wire.write(2);
-  }
-
-    Wire.endTransmission();    // stop transmitting
-
-  
+    case 'A':
+    Wire.write('A');
+    break;
+    case 'B':
+    Wire.write('B');
+    break;    
+    case 'C':
+    Wire.write('C');
+    break;
+    case 'D':
+    Wire.write('D');
+    break;
+    case 'E':
+    Wire.write('E');
+    break;
+    case 'F':
+    Wire.write('F');
+    break;
+    case 'G':
+    Wire.write('G');
+    break;
+    case 'H':
+    Wire.write('H');
+    break;    
+    case 'I':
+    Wire.write('I');
+    break;
+    case 'J':
+    Wire.write('J');
+    break;
+    case 'K':
+    Wire.write('K');
+    break;
+    case 'L':
+    Wire.write('L');
+    break;
+    case 'M':
+    Wire.write('M');
+    break;
+    case 'N':
+    Wire.write('N');
+    break;    
+    case 'O':
+    Wire.write('O');
+    break;
+    case 'P':
+    Wire.write('P');
+    break;
+    case 'Q':
+    Wire.write('Q');
+    break;
+//    case 'F':
+//    Wire.write('F');
+//    break;
+    }  
+  Wire.endTransmission();    // stop transmitting
 }
