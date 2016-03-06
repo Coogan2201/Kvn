02e75c2a9c75c0ba04c990db1d7d58ecb96694aa
Coogan2201 <c.coogan2201@gmail.com>
2016-03-06 10:37:25 -0500

    Update Pumps.h

--- PumpsGantry/Pumps.h
+++ PumpsGantry/Pumps.h
@@ -1,14 +1,35 @@
 class Pumps
 {
   public:
-  void Pump(int plate, int pumpz)//, int delayy)
+//    void Volume(int speedd, int duration)
+//  {
+//    int vol = f(speedd,duration);
+//  }
+  
+  
+  void Pump(int plate, int pumpz, int speedd, int duration)//  void Pump(int plate, int pumpz, int vol)
   {
     if(plate == 6)
     {
-      analogWrite(pumpz,120);
-      delay(5000);
+      analogWrite(pumpz,speedd);
+      delay(duration);
       analogWrite(pumpz,0);
-      delay(1000);
+      delay(2000);
+    }
+    if(plate == 24)
+    {
+      analogWrite(pumpz,speedd/4);
+      delay(duration/4);
+      analogWrite(pumpz,0);
+      delay(2000);
+    }
+    if(plate == 96)
+    {
+      analogWrite(pumpz,speedd/16);
+      delay(duration/16);
+      analogWrite(pumpz,0);
+      delay(2000);
     }
   }
+
 };
