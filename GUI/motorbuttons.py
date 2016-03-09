ce6e7268fb600f63b3ee631cd47f7e638fc07dc2
Coogan2201 <c.coogan2201@gmail.com>
2016-03-09 14:48:06 -0500

    Update motorbuttons.py

--- GUI/motorbuttons.py
+++ GUI/motorbuttons.py
@@ -1,43 +1,89 @@
 from tkinter import *
+from SrDes import wellCreator
 
+def select_size(self):
+    if(self.varWells.get() == '6'):
+        self.des.grid_forget()
+        self.des.destroy()
+        wellCreator.create_wells(self,'6')
+    if(self.varWells.get() == '24'):
+        self.des.grid_forget()
+        self.des.destroy()
+        wellCreator.create_wells(self,'24')
+    if(self.varWells.get() == '96'):
+        self.des.grid_forget()
+        self.des.destroy()
+        wellCreator.create_wells(self,'96')
+        
 def create_buttons(self):
-    self.varMotors = StringVar(self)
-    self.motorsChoice = {'X': 'X','Y': 'Y', 'Z':'Z','EX1':'EX1','EX2':'EX2'}
-    self.motorOption = OptionMenu(self, self.varMotors, *self.motorsChoice)
-    self.varMotors.set('What motors do you want to move?')
-    self.motorOption.grid(column = 0, row =0)
 
-    self.varDirection = StringVar(self)
-    self.directionChoice = {'HIGH':"1", "LOW":"0"}
-    self.directionOption = OptionMenu(self,self.varDirection,*self.directionChoice)
-    self.varDirection.set("What direction do you want to move?")
-    self.directionOption.grid(column=1,row=0)
-     
-    self.distance = Label(self,text="How far: (cm)")
-    self.distance.grid(row=0, column = 2)
-    self.distance_ = Entry(self)
-    self.distance_.grid(row = 0, column = 3, sticky = W)         
-     
-     
+    self.SelectButton=Button(self,text="Select!",command=lambda:select_size(self)).grid(column = 1, row = 4)
+    
+    self.GoButton=Button(self,text="Move!")
+    self.GoButton.grid(column=1,row=10)
+    self.GoButton.bind('<Button-1>',self.buttonPressing)
 
-    self.ForwardButton=Button(self,text="Go to home")
-    self.ForwardButton.grid(column=5,row=3)   
-    self.ForwardButton.bind('<Button-1>', self.buttonPressing)
+    self.HomeButton=Button(self,text="Go to home")
+    self.HomeButton.grid(column=5,row=3)   
+    self.HomeButton.bind('<Button-1>', self.buttonPressing)
+
+    self.MediaButton=Button(self,text="Change Media")
+    self.MediaButton.grid(column=5,row=5)   
+    self.MediaButton.bind('<Button-1>', self.buttonPressing)
+
+    self.LeftButton=Button(self,text="Move Left")
+    self.LeftButton.grid(column=7,row=7)   
+    self.LeftButton.bind('<Button-1>', self.buttonPressing)        
+    self.LeftButton.bind('<ButtonRelease-1>', self.buttonPressing)
+
+    self.RightButton=Button(self,text="Move Right")
+    self.RightButton.grid(column=9,row=7)   
+    self.RightButton.bind('<Button-1>', self.buttonPressing)        
+    self.RightButton.bind('<ButtonRelease-1>', self.buttonPressing) 
     
-    self.ForwardButton=Button(self,text="6 well plate")
-    self.ForwardButton.grid(column=4,row=4)   
-    self.ForwardButton.bind('<Button-1>', self.buttonPressing)
+    self.BackButton=Button(self,text="Move Back")
+    self.BackButton.grid(column=8,row=8)   
+    self.BackButton.bind('<Button-1>', self.buttonPressing)        
+    self.BackButton.bind('<ButtonRelease-1>', self.buttonPressing) 
     
-    #Well plate selector
+    self.ForwardButton=Button(self,text="Move Forward")
+    self.ForwardButton.grid(column=8,row=6)   
+    self.ForwardButton.bind('<Button-1>', self.buttonPressing)        
+    self.ForwardButton.bind('<ButtonRelease-1>', self.buttonPressing) 
+    
+    self.UpButton=Button(self,text="Move Up")
+    self.UpButton.grid(column=9,row=6)   
+    self.UpButton.bind('<Button-1>', self.buttonPressing)        
+    self.UpButton.bind('<ButtonRelease-1>', self.buttonPressing) 
+    
+    self.DownButton=Button(self,text="Move Down")
+    self.DownButton.grid(column=9,row=8)   
+    self.DownButton.bind('<Button-1>', self.buttonPressing)        
+    self.DownButton.bind('<ButtonRelease-1>', self.buttonPressing)         
+    
+    self.EtOHButton=Button(self,text="Ethanol Out")
+    self.EtOHButton.grid(column=10,row=10)   
+    self.EtOHButton.bind('<Button-1>', self.buttonPressing)        
+    self.EtOHButton.bind('<ButtonRelease-1>', self.buttonPressing)         
+    
+    self.WasteButton=Button(self,text="Waste In")
+    self.WasteButton.grid(column=10,row=11)   
+    self.WasteButton.bind('<Button-1>', self.buttonPressing)        
+    self.WasteButton.bind('<ButtonRelease-1>', self.buttonPressing)         
+        
+        
+        
+        #Well plate selector
     self.varWells = StringVar(self)
-    self.wellChoice = {'6': '6','12': '12', '24':'24','96':'96'}
+    self.wellChoice = {'6': '6', '24':'24','96':'96'}
     self.wellOption = OptionMenu(self, self.varWells, *self.wellChoice)
     self.varWells.set('What size plate are you using?')
-    self.wellOption.grid(column = 3, row =5)
-    
+    self.wellOption.grid(column = 1, row =5)
+        
     #Protocol
-    self.varProtocol = StringVar(self)
-    self.protocolChoice = {'Aspirate': 'Aspirate','Media': 'Media', 'PBS':'PBS','Trypsin':'Trypsin','EthanolWash':'EthanolWash'}
-    self.protocolOption = OptionMenu(self, self.varProtocol, *self.protocolChoice)
-    self.varProtocol.set('What liquid do you want?')
-    self.protocolOption.grid(column = 3, row =6)
+#    self.varProtocol = StringVar(self)
+#    self.protocolChoice = {'Aspirate': 'Aspirate','Media': 'Media', 'PBS':'PBS','Trypsin':'Trypsin','EthanolWash':'EthanolWash'}
+#    self.protocolOption = OptionMenu(self, self.varProtocol, *self.protocolChoice)
+#    self.varProtocol.set('What liquid do you want?')
+#    self.protocolOption.grid(column = 3, row =6)
+    
